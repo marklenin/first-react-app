c98c03ef76e58ca9d8e44cddcd7e64fa56e3a29f
zhryskulova <ryskulovajanayim@gmail.com>
2023-05-17 21:16:56 +0600

    zhanayim commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 import Navbar from "./components/navbar/Navbar";
+import CheckoutPage from "./components/checkout/checkout";
 
 function App() {
   return (
     <div>
       <Navbar />
       <MainRoutes />
+      <CheckoutPage/>
     </div>
   );
 }
